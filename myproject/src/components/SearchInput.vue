338fb4c6b42a48cc6b8735eb4ceaa56209769dd6
hoaithu <hoaithu.hb2002@gmail.com>
2026-08-18 16:40:25 +0700

    5.1: practice mini exercise

---
+++ myproject/src/components/SearchInput.vue
@@ -0,0 +1,7 @@
+<template>
+  <input type="text" placeholder="Enter..." aria-label="input text" v-model="model" />
+</template>
+<script>
+import { defineModel } from 'vue'
+const model = defineModel()
+</script>
---
+++ myproject/src/components/SearchPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import SearchInput from './SearchInput.vue';
+
+</script>
+
+<template>
+    Search Page
+    <SearchInput />
+</template>
