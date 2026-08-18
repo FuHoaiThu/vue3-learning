971209d3dc4cef5e6914b06dc1bd054db17818d8
hoaithu <hoaithu.hb2002@gmail.com>
2026-08-18 22:30:59 +0700

    04: components and props

---
+++ myproject/src/components/products/ProductCart.vue
@@ -0,0 +1,44 @@
+<template>
+  <li class="card">
+    <div class="card-header">
+      <img :src="image" alt="card image" />
+      <h2>{{ name }}</h2>
+    </div>
+    <p>{{ price }}</p>
+    <button @click="onAdd">Add to cart</button>
+  </li>
+</template>
+<script setup>
+const props = defineProps({
+  image: {
+    type: String,
+    default: '',
+  },
+  name: {
+    type: String,
+    default: '',
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  id: {
+    type: Number,
+    default: 0,
+  },
+})
+const emit = defineEmits(['add-to-cart'])
+
+const onAdd = () => {
+  emit('add-to-cart', props.id)
+}
+</script>
+<style lang="css" scoped>
+.card {
+  overflow: hidden;
+}
+.card-header img {
+  width: 100%;
+  height: 300px;
+}
+</style>
