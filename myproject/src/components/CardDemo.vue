07feb0fb3f17af45873e56cc3e6c7b11df89deed
hoaithu <hoaithu.hb2002@gmail.com>
2026-08-19 14:48:16 +0700

    05.3: practice named slot

---
+++ myproject/src/components/CardDemo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <BaseCard>
+      <template #header>
+        <h2>Mechanical Keyboard</h2>
+      </template>
+      <p>Price: 150.000đ</p>
+      <template #footer>
+        <button>Add to cart</button>
+      </template>
+    </BaseCard>
+  </div>
+</template>
+<script setup>
+import BaseCard from './BaseCard.vue'
+</script>
