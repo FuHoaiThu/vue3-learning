ffaab895d2085e552f4573a632674f2df1a91e24
hoaithu <hoaithu@gmail.com>
2026-08-19 13:31:29 +0700

    05.1: practice component model and defineModel

--- myproject/src/components/SearchInput.vue
+++ myproject/src/components/SearchInput.vue
@@ -1,7 +1,6 @@
 <template>
   <input type="text" placeholder="Enter..." aria-label="input text" v-model="model" />
 </template>
-<script>
-import { defineModel } from 'vue'
+<script setup>
 const model = defineModel()
 </script>
--- myproject/src/components/SearchPage.vue
+++ myproject/src/components/SearchPage.vue
@@ -1,9 +1,10 @@
-<script setup lang="ts">
-import SearchInput from './SearchInput.vue';
-
+<script setup>
+import { ref } from 'vue'
+import SearchInput from './SearchInput.vue'
+const search = ref('')
 </script>
 
 <template>
-    Search Page
-    <SearchInput />
+  <SearchInput v-model="search" />
+  <p>Keyword: {{ search }}</p>
 </template>
