e7504f46990b86423adea903d708c856ce8c724e
hoaithu <hoaithu.hb2002@gmail.com>
2026-08-19 13:50:09 +0700

    05.2: practice basic slot

---
+++ myproject/src/components/CardDemo.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import BaseCard from './BaseCard.vue'
+</script>
+
+<template>
+  <div>
+    <h1>Slot</h1>
+    <BaseCard>
+      <h2>Nguyen Van An</h2>
+      <p>Frontend Developer</p>
+    </BaseCard>
+    <BaseCard>
+        <h2>Mechanical Keyboard</h2>
+        <p>Price: 150.000đ</p>
+        <button>Add to cart</button>
+    </BaseCard>
+  </div>
+</template>
