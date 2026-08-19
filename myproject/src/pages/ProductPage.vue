27c5660e1765890eb61c27a59aa1b002613e81e0
hoaithu <hoaithu.hb2002@gmail.com>
2026-08-19 15:42:50 +0700

    05: mock product page

---
+++ myproject/src/pages/ProductPage.vue
@@ -0,0 +1,36 @@
+<script setup>
+const products = ref([
+  {
+    id: 1,
+    name: 'Mechanical Keyboard',
+    price: 150000,
+    category: 'Keyboard',
+  },
+  {
+    id: 2,
+    name: 'Gaming Mouse',
+    price: 200000,
+    category: 'Mouse',
+  },
+  {
+    id: 3,
+    name: 'Gaming Monitor',
+    price: 3000000,
+    category: 'Monitor',
+  },
+  {
+    id: 4,
+    name: 'Wireless Keyboard',
+    price: 500000,
+    category: 'Keyboard',
+  },
+])
+</script>
+<template>
+  <div class="product">
+    <h1>Product Browser</h1>
+    <div class="product-search">
+        <input type="text" placeholder="Search product... "/>
+    </div>
+  </div>
+</template>
