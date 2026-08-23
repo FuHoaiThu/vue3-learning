862cedcb9d3052a686de3692b670341b59b4d068
hoaithu <hoaithu.hb2002@gmail.com>
2026-08-23 22:03:15 +0700

    06.1: composition API, script setup; practice with ref, reactive and computerd

--- myproject/src/components/Greeting.vue
+++ myproject/src/components/Greeting.vue
@@ -1,31 +1,22 @@
-<!-- <script>
-export default {
-  data() {
-    return {
-      firstName: 'Hoai',
-      lastName: 'Thu',
-    }
-  },
-
-  methods: {
-    sayHello() {
-      console.log(
-        `Hello ${this.firstName} ${this.lastName}`
-      )
-    },
-
-    changeName() {
-      this.firstName = 'Anna'
-    }
-  }
-}
-</script> -->
-
 <script setup>
-import { ref } from 'vue'
+import { computed, reactive, ref, toRefs } from 'vue'
 
-const firstName = ref('Thu')
-const lastName = ref('Hoai')
+const count = ref(0)
+const isVisible = ref(true)
+const currentYear = ref(new Date().getFullYear())
+const user = reactive({
+  firstName: 'Thu',
+  lastName: 'Hoai',
+  age: 20,
+})
+const { firstName, lastName, age } = toRefs(user)
+
+const fullName = computed(() => {
+  return `${firstName.value} ${lastName.value}`
+})
+const birthYear = computed(() => {
+  return currentYear.value - age.value
+})
 
 const sayHello = () => {
   console.log(`Hello ${firstName.value} ${lastName.value}`)
@@ -33,12 +24,30 @@ const sayHello = () => {
 const changeName = () => {
   firstName.value = 'Onyra'
 }
+const handleIncreaseAge = () => {
+  age.value++
+}
+const handleIncreaseCount = () => {
+  count.value++
+}
+const handleToggleInfo = () => {
+  isVisible.value = !isVisible.value
+}
 </script>
 
 <template>
-  <h1>{{ firstName }} {{ lastName }}</h1>
+  <h1>Greetings</h1>
+  <div class="info" v-show="isVisible">
+    <p>Name: {{ fullName }}</p>
+    <p>Age: {{ age }}</p>
+    <p>Count: {{ count }}</p>
+    <p>BirthYear: {{ birthYear }}</p>
+  </div>
 
   <button @click="sayHello">Say Hello</button>
 
   <button @click="changeName">Change name</button>
+  <button @click="handleIncreaseAge">Increase Age</button>
+  <button @click="handleIncreaseCount">Increase count</button>
+  <button @click="handleToggleInfo">Toggle information</button>
 </template>
