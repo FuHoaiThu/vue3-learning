6a41c7165a3b01768fd6baf1ec3a0a8c4515adf4
hoaithu <hoaithu.hb2002@gmail.com>
2026-08-23 22:14:13 +0700

    06.2 practice watch

---
+++ myproject/src/components/ProductSearch.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input v-model="keyword" type="text" placeholder="Search..." aria-label="product search" />
+    <select>
+      <option></option>
+    </select>
+  </div>
+</template>
+<script setup>
+import { ref, watch } from 'vue'
+
+const keyword = ref('')
+
+const searchProducts = (keyword) => {
+  console.log(`Searching products with keyword: ${keyword}`)
+}
+
+watch(keyword, (newValue, oldValue) => {
+  console.log(`Keyword changed from "${oldValue}" to "${newValue}"`)
+})
+</script>
