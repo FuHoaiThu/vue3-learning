37f3e72d77da33134794535857e6519fba31896e
hoaithu <hoaithu.hb2002@gmail.com>
2026-08-24 14:23:16 +0700

    06.3: lifecyle hooks

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -1,11 +1,15 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import LifecycleDemo from './components/LifecycleDemo.vue'
+
+const isShow = ref(false)
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <LifecycleDemo v-if="isShow" />
+    <button @click="isShow = !isShow">Toggle</button>
+  </div>
 </template>
 
 <style scoped></style>
