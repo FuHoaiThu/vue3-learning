77ca6fb0ea9c48f41d5c8deeafdbd9cd1b55b9ed
hoaithu <hoaithu.hb2002@gmail.com>
2026-08-24 13:31:10 +0700

    06.2: practice watch with many state

--- myproject/src/components/ProductSearch.vue
+++ myproject/src/components/ProductSearch.vue
@@ -1,21 +1,33 @@
 <template>
   <div>
-    <input v-model="keyword" type="text" placeholder="Search..." aria-label="product search" />
-    <select>
-      <option></option>
-    </select>
+    <div>
+      <input v-model="keyword" type="text" placeholder="Search..." aria-label="product search" />
+    </div>
+    <div>
+      <select id="category" v-model="category">
+        <option value="all">All</option>
+        <option value="phone">Phone</option>
+        <option value="laptop">Laptop</option>
+      </select>
+    </div>
+    <div>
+      <button @click="page--">Previous</button>
+      <button @click="page++">Next</button>
+    </div>
   </div>
 </template>
 <script setup>
 import { ref, watch } from 'vue'
 
 const keyword = ref('')
+const category = ref('all')
+const page = ref(1)
 
-const searchProducts = (keyword) => {
-  console.log(`Searching products with keyword: ${keyword}`)
+const searchProducts = (keyword, category, page) => {
+  console.log(`Searching: keyword="${keyword}", category="${category}", page=${page}`)
 }
 
-watch(keyword, (newValue, oldValue) => {
-  console.log(`Keyword changed from "${oldValue}" to "${newValue}"`)
+watch([keyword, category, page], ([newKeyword, newCategory, newPage], [oldKeywor, oldCategory, oldPage]) => {
+  searchProducts(newKeyword, newCategory, newPage)
 })
 </script>
