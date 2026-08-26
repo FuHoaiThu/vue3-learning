4a198750e155a7616ff6bcdf373dc91249caf113
hoaithu <hoaithu.hb2002@gmail.com>
2026-08-26 21:25:25 +0700

    07: practice router

--- myproject/src/router/index.js
+++ myproject/src/router/index.js
@@ -1,8 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/pages/Home.vue'),
+  },
+  {
+    path: '/products',
+    name: 'Producs',
+    component: () => import('@/pages/Products.vue'),
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('@/pages/About.vue'),
+  },
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
